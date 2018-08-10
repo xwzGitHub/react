36eb60245dc108c195025efbd14778c7f7a08a90
xwzGitHub <695015479@qq.com>
2018-08-10 13:52:01 +0800

    大神案例QQ音乐

---
+++ react-mobile-qqMusic/src/main.js
@@ -0,0 +1,26 @@
+/**
+ * Created by wuming on 2017/7/11.
+ */
+import './utils/antm-viewport.min';
+import './assets/css/reset.css';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, Redirect } from 'react-router';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Home from './views/Home/Home';
+// import createBrowserHistory from 'history/createBrowserHistory';
+import configureStore from '../src/store/configureStore';
+// const history = createBrowserHistory();
+const store = configureStore();
+render(
+    <Provider store={store}>
+         {/* <Router history={history}>
+            <Route path="/" component={Home}></Route>
+        </Router> */}
+        <HashRouter>
+            <Route path="/" component={Home}></Route>
+        </HashRouter>
+    </Provider>,
+    document.getElementById("app")
+);
